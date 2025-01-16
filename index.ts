48dcde051a8d43e7c9fba1cc992d30caf64184b6
leocodeio <basicleo787@gmail.com>
2025-01-16 19:16:31 +0000

    fix: added entrypoint

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { SkipLogging } from 'src/logging/logging.decorator';
-import { LoggingModule } from 'src/logging/logging.module';
-import { LoggingInterceptor } from 'src/logging/logging.interceptor';
-
-export { LoggingModule, SkipLogging, LoggingInterceptor };
